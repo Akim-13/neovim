b079ab71e5aca2a66e3a69a189523db6deaff676
Akim <akimkom@protonmail.com>
2022-05-21 21:25:34 +0100

    Fixed indentation and minor changes

--- lua/rc/undotree.lua
+++ lua/rc/undotree.lua
@@ -3,6 +3,7 @@ if not status_ok then
     return
 end
 
+-- FIXME: the window somethimes opens on the LHS instead of RHS.
 vim.g.undotree_WindowLayout = 3
 vim.g.undotree_HighlightChangedText = 0
 vim.g.undotree_ShortIndicators = 1
